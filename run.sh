484ab2f693625149cec15f5e1dfcdc532559dc83
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2023-08-24 22:30:29 +0930

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,15 +12,15 @@ do
 	  echo '-------------------------------------------------------------'
 	  aws cloudformation create-stack --stack-name troubleshoot --template-body "file://TroubleshootingCLD401.yml" >/dev/null 2>&1
 	  aws cloudformation wait stack-create-complete --stack-name troubleshoot
-	  echo '-------------------------------------------------------------'
-	  echo '	Setup Completed You can start the troubleshooting    '
-	  echo '-------------------------------------------------------------'
    	  sleep 70
 	  TRtable=$(aws ec2 describe-route-tables --filters "Name=tag:Name,Values=TR PublicRouteTable" "Name=vpc-id,Values=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=TroubleshootingVPC" --query "Vpcs[0].VpcId" --output text)" --query "RouteTables[0].RouteTableId" --output text)
 	  TRgw=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=TroubleshootingGW" --query "InternetGateways[0].InternetGatewayId" --output text)
 	  aws ec2 delete-route --route-table-id $TRtable --destination-cidr-block 0.0.0.0/0 >/dev/null 2>&1
 	  aws ec2 create-route --route-table-id $TRtable --destination-cidr-block 10.10.0.0/16 --gateway-id $TRgw >/dev/null 2>&1
-	  echo "Below are the related information for your reference"
+   	  echo '-------------------------------------------------------------'
+	  echo '	Setup Completed You can start the troubleshooting    '
+	  echo '-------------------------------------------------------------'
+	  echo "Below is the related information for your reference"
 	  aws cloudformation describe-stacks --stack-name troubleshoot --query "Stacks[*].Outputs[*].{OutputKey: OutputKey, OutputValue: OutputValue, Description: Description}" --output table
 	  aws ec2 describe-instances --filter "Name=tag:Name,Values=Troubleshooting-server" --filters "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].{InstanceId:InstanceId, PrivateIpAddress:PrivateIpAddress, PublicIpAddress:PublicIpAddress}" --output table
 	  break
